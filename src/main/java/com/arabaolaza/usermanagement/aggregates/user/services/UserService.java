8628ccff541d79b9416edd69db93cae9c8bebf5a
raul <raul.arabaolaza@getsetpay.com>
2016-03-06 22:50:43 +0100

    First attemp at creating a find details for Aggregate

--- src/main/java/com/arabaolaza/usermanagement/aggregates/user/services/UserService.java
+++ src/main/java/com/arabaolaza/usermanagement/aggregates/user/services/UserService.java
@@ -12,14 +12,19 @@ import com.arabaolaza.usermanagement.aggregates.user.commands.UserCommand;
 
 import net.chrisrichardson.eventstore.EntityIdentifier;
 import net.chrisrichardson.eventstore.EntityWithIdAndVersion;
+import net.chrisrichardson.eventstore.EventStore;
 import net.chrisrichardson.eventstore.repository.AggregateRepository;
+import net.chrisrichardson.eventstore.util.ServiceUtil;
 import rx.Observable;
 
 @Service
 public class UserService {
 	@Autowired
 	private AggregateRepository<User, UserCommand> userRepository;
-
+	
+	@Autowired
+	private EventStore store;
+	
 	private final Random random = new Random(System.currentTimeMillis());
 
 	public Observable<EntityWithIdAndVersion<User>> createUser(Integer age, String name, String lastName) {
@@ -28,7 +33,15 @@ public class UserService {
 	}
 	
 	public Observable<EntityWithIdAndVersion<User>> updateUserName(EntityIdentifier identifier, String newName) {
-		return userRepository.update(identifier,new UpdateUserNameCommand(newName));
+		Observable<EntityWithIdAndVersion<User>> toReturn=userRepository.update(identifier,new UpdateUserNameCommand(newName));
+		toReturn.subscribe((u) -> System.out.println(u));
+		return toReturn;
+		
+	}
+	
+	public Observable<EntityWithIdAndVersion<User>> findDetails(EntityIdentifier identifier){
+		return store.find(User.class, identifier).map((u)-> new EntityWithIdAndVersion<User>(u.entityIdAndVersion(),new User()));
+		
 	}
 
 }
